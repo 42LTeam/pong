bb8d8f0101bc37334a6f7070e06c7165b473c0a8
Ubuntu <ubuntu@ov-059a58.ch3.infomaniak.ch>
2023-06-21 14:49:07 +0000

    working back end service in docker-compose

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
