925b0a8bc80f4bf22d82b5d3069d8b742a67153a
Eduardo Sanchez <esanchez@student.42lausanne.ch>
2023-07-18 03:51:19 +0200

    changes in prisma services/controllers

--- back/src/match/match.controller.ts
+++ back/src/match/match.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Get, Param, Post, Put, Body } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { MatchService } from './match.service';
 import { Match } from '@prisma/client';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 class CreateMatchDto {
     @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
     usersIds: number[];
   
     @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
     scores: number[];
   }
   
