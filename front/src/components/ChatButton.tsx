5872a4a147184a997b33ea13cc77307490818780
esanchez <esanchez@student.42lausanne.ch>
2023-07-20 18:39:20 +0200

    Auto stash before merge of "esanchez" and "origin/shai"

---
+++ front/src/components/ChatButton.tsx
@@ -0,0 +1,7 @@
+import "../css/chat.css"
+
+export default function ChatButton() {
+    return (
+        <img alt="A svg file representing chat logo" className="chat-bubble" src="/svg/chatBubble.svg"/>
+    )
+}
