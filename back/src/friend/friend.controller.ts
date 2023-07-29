fe9b7f1a7d2bde0a805947d246627e628873c2fc
Shai Halimi <shai.halimi@gmail.com>
2023-07-29 03:56:08 +0200

    fixed sendFriendRequest endpoint

--- back/src/friend/friend.controller.ts
+++ back/src/friend/friend.controller.ts
@@ -4,12 +4,21 @@ import { FriendService } from './friend.service';
 import { UserFriendship,  User } from '@prisma/client';
 import { IsNotEmpty, IsNumber } from '@nestjs/class-validator';
 import {AuthenticatedGuard} from "../auth/guards/authenticated.guard";
+import {Roles} from "../auth/roles.decorator";
 
-class CreateFriendRequestDto {
+class AdminCreateFriendRequestDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   initiatorId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  acceptorId: number;
+}
+
+class CreateFriendRequestDto {
 
   @ApiProperty()
   @IsNotEmpty()
@@ -24,13 +33,26 @@ class CreateFriendRequestDto {
 export class FriendController {
   constructor(private friendService: FriendService) {}
 
-  @Post('friend-request')
+  @Post('friend-request/admin')
+  @Roles(1)
   @ApiOperation({ summary: 'Create a friend request' })
-  @ApiBody({ type: CreateFriendRequestDto })
+  @ApiBody({ type: AdminCreateFriendRequestDto })
   async createFriendRequest(
-    @Body() createFriendRequestDto: CreateFriendRequestDto): Promise<UserFriendship> {
+    @Body() createFriendRequestDto: AdminCreateFriendRequestDto): Promise<UserFriendship> {
     return this.friendService.createFriendRequest(createFriendRequestDto.initiatorId, createFriendRequestDto.acceptorId);
   }
+
+  @Post('friend-request')
+  @Roles(0)
+  @ApiOperation({ summary: 'Create a friend request' })
+  @ApiBody({ type: CreateFriendRequestDto })
+  async sendFriendRequest(
+      @Req() req: any,
+      @Body() createFriendRequestDto: CreateFriendRequestDto): Promise<UserFriendship> {
+    const user = await req.user;
+    return this.friendService.createFriendRequest(user.id, createFriendRequestDto.acceptorId);
+  }
+
 
 
   @Put('friend-request/accept/:friendshipId')
