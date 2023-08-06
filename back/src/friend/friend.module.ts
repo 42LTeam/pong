f68c94b2a6a4ba298d1e2985cbf3fb9b21421c66
Shai <shai.halimi@gmail.com>
2023-08-06 22:01:22 +0200

    fixed compilation problem

--- back/src/friend/friend.module.ts
+++ back/src/friend/friend.module.ts
@@ -2,7 +2,7 @@ import {Module} from "@nestjs/common";
 import {FriendController} from "./friend.controller";
 import {FriendService} from "./friend.service";
 import {PrismaModule} from "../prisma/prisma.module";
-import { AdminController } from './admin/admin.controller';
+import {AdminController} from "./admin/admin.controller";
 
 @Module({
     controllers: [FriendController, AdminController],
