bb49f3e70a31997f4438d51a0fc330af55a30297
Shai Halimi <shai.halimi@gmail.com>
2023-08-09 02:30:43 +0200

    added avatar in conversation

--- front/src/components/friend/Friend.tsx
+++ front/src/components/friend/Friend.tsx
@@ -1,9 +1,9 @@
 import {Children} from "react";
+import Avatar from "../utils/Avatar";
 
 type Props = {
     friend: any,
     onClick?: any,
-    userId?: number,
     children?: any,
 }
 
@@ -12,7 +12,8 @@ export default function Friend(props: Props){
 
     return (
         <div onClick={() => {props.onClick ? props.onClick(props.friend) : null}} className="friend">
-            <div className="conversation-avatar"></div>
+            <Avatar width="48px" height="48px" url={props.friend?.avatar}></Avatar>
+
             <div className="conversation-content">
                 <div className="conversation-username">{props.friend?.username}</div>
                 <div className="conversation-preview">{props.friend?.status}</div>
