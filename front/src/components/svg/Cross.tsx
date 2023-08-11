4d0e0866b5b799262fc28492d615b68d000480c7
Shai Halimi <shai.halimi@gmail.com>
2023-08-12 01:11:58 +0200

    created cross svg

---
+++ front/src/components/svg/Cross.tsx
@@ -0,0 +1,8 @@
+export default function Cross(props: { color: string } }){
+    return (
+        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 2.11143L14.3886 0.5L8 6.88857L1.61143 0.5L0 2.11143L6.38857 8.5L0 14.8886L1.61143 16.5L8 10.1114L14.3886 16.5L16 14.8886L9.61143 8.5L16 2.11143Z"
+                  fill={props.color || "#7F8C8D"}/>
+        </svg>
+    )
+}
