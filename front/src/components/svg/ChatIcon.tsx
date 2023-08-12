6bed3bfddd02d32356eca79bd57575a7f0b8dacc
rmonney <rmonney@student.42lausanne.ch>
2023-08-12 02:09:21 +0200

    fix header navigation icons and css renamed components "Button" that were more "Icon"

--- front/src/components/svg/ChatIcon.tsx
+++ front/src/components/svg/ChatIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import "../../css/chat.css"
 
-export default function ChatButton() {
+export default function ChatIcon() {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
         <path d="M0 0h24v24H0z" fill="none"/>
