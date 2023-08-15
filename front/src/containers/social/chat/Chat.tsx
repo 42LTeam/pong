92f69f006bfe02e84c52e8b2e5a03c42016f2a09
shalimi <Shai.halimi@gmail.com>
2023-08-15 02:16:49 +0200

    finally got messages working

--- front/src/containers/social/chat/Chat.tsx
+++ front/src/containers/social/chat/Chat.tsx
@@ -18,21 +18,43 @@ export default function Chat (props:ChatProps){
     const user = useContext(AuthContext);
     const application = useContext(ApplicationContext);
     const ref = useRef(null);
+    const toAdd = application.social.newMessages.filter(current => current.channelId == channel && messages.map(c => c.id).includes(current.id) == false);
+
+
+    const fetchData = async () => {
+        console.log('mount2')
+        const response = await getChannelMessages(props.channel);
+        const data = response.data;
+        const tmp_messages = [...data.messages].reverse();
+        setMessages(tmp_messages);
+        setLastRead(data.lastRead);
+        readMessage(props.channel, tmp_messages[0].id);
+    }
 
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await getChannelMessages(props.channel);
-            const data = response.data;
-            setLastRead(data.lastRead);
-            setMessages([...data.messages].reverse());
+        console.log('mount1');
+        fetchData();
+        return () => {
+            setMessages([]);
+            application.clearMessage(toAdd);
+            console.log('destroyed')
         }
-        fetchData().catch(console.error);
     }, [channel]);
-    const toAdd = application.social.newMessages.filter(current => current.channelId == channel && messages.map(c => c.id).includes(current.id) == false);
-    if (toAdd.length) {
-        setMessages([...toAdd, ...messages]);
-        readMessage(props.channel, toAdd[toAdd.length - 1].id)
-    }
+
+
+    useEffect(() => {
+        if (toAdd.length) {
+            console.log('condition')
+            const last = toAdd[toAdd.length - 1];
+            setMessages([...toAdd, ...messages]);
+            readMessage(props.channel, last.id).then(() => {
+            });
+            if (last.userId == user.id) {
+                setLastRead(last.id);
+            }
+        }
+    }, [application]);
+
 
     if (props.channel != channel)
         setChannel(props.channel);
@@ -40,23 +62,66 @@ export default function Chat (props:ChatProps){
         if (!ref || !ref.current.value) return;
         if (event.key != null && event.key != 'Enter') return;
         await sendMessageToChannel(channel, ref.current.value);
+
         ref.current.value = null;
     }
 
+    const unReadMessages = messages.filter(current => {
+        return (current.id > lastRead)
+    });
+
     return (
         <div className="chat-root">
-            {(JSON.stringify(lastRead || ''))}
+            {lastRead}
             <div className="chat-messages">
-                {messages.filter((value, index, array) => array.indexOf(value) === index).map((current) => {
+
+                {unReadMessages.map((current) => {
                     return (
-                        <Message
-                            key={current.id}
-                            sender={current.user?.avatar}
-                            content={current.content}
-                            date={new Date(current.created_at).toTimeString().slice(0,5)}
-                            sent={current.userId == user.id}></Message>
+                        <>
+                            <Message
+                                key={current.id}
+                                sender={current.user?.avatar}
+                                content={current.content + ' ' + current.id}
+                                date={new Date(current.created_at).toTimeString().slice(0,5)}
+                                sent={current.userId == user.id}
+                            ></Message>
+                        </>
+
                     )
                 })}
+                {unReadMessages.length ? <div className="row">
+                    <h2 style={{
+                        background: '#E74C3C',
+                        borderRadius: '10px',
+                        paddingLeft: '3px',
+                        paddingRight: '3px',
+                    }}>Nouveaux</h2>
+                    <div className="horizontal-separator" style={{
+                        background: '#E74C3C',
+                        alignSelf: 'center',
+                        flexShrink: 0,
+                        flexGrow: 1,
+                    }}></div>
+                </div>: null}
+                {messages.filter(current => {
+                    return (current.id <= lastRead)
+                }).map((current) => {
+                    return (
+                        <>
+                            <Message
+                                key={current.id}
+                                sender={current.user?.avatar}
+                                content={current.content + ' ' + current.id}
+                                date={new Date(current.created_at).toTimeString().slice(0,5)}
+                                sent={current.userId == user.id}
+                            ></Message>
+                        </>
+
+                    )
+                })}
+
+
+
             </div>
             <TextInput ref={ref} color="#7F8C8D" text="Votre message..." bgColor="#ECF0F1" onKeyDown={handleSendMessage} button={<Send handleClick={handleSendMessage}></Send>}></TextInput>
         </div>
