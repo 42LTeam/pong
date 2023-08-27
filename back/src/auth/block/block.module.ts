73b54299bed91efa74b2befd3cd6f40f372960b1
esanchez <esanchez@student.42lausanne.ch>
2023-08-27 16:34:07 +0200

    fixed typos

--- back/src/auth/block/block.module.ts
+++ back/src/auth/block/block.module.ts
@@ -1,8 +1,8 @@
 import {Module} from "@nestjs/common";
 import {BlockController} from "./block.controller";
 import {BlockService} from "./block.service";
-import {PrismaModule} from "../prisma/prisma.module";
-import {FriendModule} from "../friend/friend.module";
+import {PrismaModule} from "../../prisma/prisma.module";
+import {FriendModule} from "../../friend/friend.module";
 
 @Module({
     controllers: [BlockController],
