379069c2da1ccdc111fc38f60200d78904b9088a
Lowell <zimapro@protonmail.com>
2023-08-29 20:05:02 +0200

    Pass test - make prettier to make it work , change back to pull request

--- front/src/containers/HomePage/PlayerSkin.tsx
+++ front/src/containers/HomePage/PlayerSkin.tsx
@@ -4,7 +4,7 @@ import "../../css/homepage.css";
 
 export const PlayerSkin = () => {
   const handleClickLeft = () => {
-      console.log("click on gauche #PlayerSkin");
+    console.log("click on gauche #PlayerSkin");
   };
   const handleClickRight = () => {
     console.log("click on droite #PlayerSkin");
@@ -13,7 +13,9 @@ export const PlayerSkin = () => {
   return (
     <div className="player-skin">
       <img
-        className="vector" alt="Vector" src="/svg/vector-left.svg"
+        className="vector"
+        alt="Vector"
+        src="/svg/vector-left.svg"
         onClick={handleClickLeft}
       />
       <img className="rectangle" alt="Rectangle" src="/svg/barre.svg" />
