19d1a6f9468e076c7a1a59c5e9853de23f1c098a
rmonney <rmonney@student.42lausanne.ch>
2023-08-31 17:34:22 +0200

    added explicit for user does not exist

--- front/src/containers/profilepage/ProfilePage.tsx
+++ front/src/containers/profilepage/ProfilePage.tsx
@@ -42,7 +42,11 @@ export default function ProfilePage(){
             });
     },[userID]);
 
-    if (user === null || !matches){
+    if (user===null){
+        return(<h1>User with ID={userID} does not exists</h1>)
+    }
+
+    if (!matches){
         return(<h1>LOADING</h1>)
     }
 
