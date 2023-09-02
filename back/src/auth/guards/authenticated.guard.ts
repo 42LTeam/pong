32bef76880ffab54aa1cf7c02029a445647cc5c5
shalimi <Shai.halimi@gmail.com>
2023-09-02 19:38:37 +0200

    forogot to commit new guard xD

--- back/src/auth/guards/authenticated.guard.ts
+++ back/src/auth/guards/authenticated.guard.ts
@@ -1,11 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 
+
+
+@Injectable()
+export class TotpGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+    async canActivate(context: ExecutionContext): Promise<any>{
+        const req = context.switchToHttp().getRequest();
+        const role = this.reflector.get<number>('roles', context.getHandler());
+        if (!role) {
+            return req.isAuthenticated();
+        }
+        const user = await req.user;
+        return req.isAuthenticated() && user.role >= role;
+    }
+
+}
+
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
-
-
     async canActivate(context: ExecutionContext): Promise<any>{
         const req = context.switchToHttp().getRequest();
         const role = this.reflector.get<number>('roles', context.getHandler());
@@ -13,8 +28,7 @@ export class AuthenticatedGuard implements CanActivate {
           return req.isAuthenticated();
         }
         const user = await req.user;
-
-        return req.isAuthenticated() && user.role >= role;
+        return req.isAuthenticated() && user.role >= role && req.session.totp;
     }
 
 }
