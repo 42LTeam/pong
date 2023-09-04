036225f19ec7dda95191af16b16ccaf2d160729a
esanchez <esanchez@student.42lausanne.ch>
2023-09-04 11:07:57 +0200

    fixed console log error

--- front/src/containers/social/chat/Chat.tsx
+++ front/src/containers/social/chat/Chat.tsx
@@ -1,17 +1,17 @@
 import TextInput from "../../../components/utils/TextInput";
-import {useContext, useEffect,  useRef, useState} from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Message from "../../../components/chat/Message";
-import {AuthContext} from "../../Auth";
+import { AuthContext } from "../../Auth";
 import "../../../css/chatBody.css"
 import Send from "../../../components/svg/Send";
-import {getChannelMessages, readMessage, sendMessageToChannel} from "../../../api";
-import {ApplicationContext} from "../../Application";
+import { getChannelMessages, readMessage, sendMessageToChannel } from "../../../api";
+import { ApplicationContext } from "../../Application";
 
 interface ChatProps {
     channel: any,
 }
 
-export default function Chat (props:ChatProps){
+export default function Chat(props: ChatProps) {
     const [messages, setMessages] = useState([]);
     const [channel, setChannel] = useState<number | null>(props.channel);
     const [lastRead, setLastRead] = useState(0);
@@ -20,7 +20,6 @@ export default function Chat (props:ChatProps){
     const ref = useRef(null);
     const toAdd = application.social.newMessages.filter(current => current.channelId == channel && messages.map(c => c.id).includes(current.id) == false);
 
-
     const fetchData = async () => {
         console.log('mount2')
         const response = await getChannelMessages(props.channel);
@@ -28,7 +27,7 @@ export default function Chat (props:ChatProps){
         const tmp_messages = [...data.messages].reverse();
         setMessages(tmp_messages);
         setLastRead(data.lastRead);
-        if( tmp_messages.length)
+        if (tmp_messages.length)
             readMessage(props.channel, tmp_messages[0].id);
     }
 
@@ -40,22 +39,20 @@ export default function Chat (props:ChatProps){
         }
     }, [channel]);
 
-
     useEffect(() => {
         if (toAdd.length) {
             const last = toAdd[toAdd.length - 1];
             setMessages([...toAdd, ...messages]);
-            readMessage(props.channel, last.id).then(() => {
-            });
+            readMessage(props.channel, last.id).then(() => { });
             if (last.userId == user.id) {
                 setLastRead(last.id);
             }
         }
     }, [application]);
 
-
     if (props.channel != channel)
         setChannel(props.channel);
+
     const handleSendMessage = async (event) => {
         if (!ref || !ref.current.value) return;
         if (event.key != null && event.key != 'Enter') return;
@@ -72,60 +69,51 @@ export default function Chat (props:ChatProps){
         <div className="chat-root">
             <div className="chat-messages">
 
-                {unReadMessages.map((current) => {
-                    return (
-                        <>
-                            <Message
-                                key={current.id}
-                                sender={current.user}
-                                content={current.content}
-                                date={new Date(current.created_at).toTimeString().slice(0,5)}
-                                sent={current.userId == user.id}
-                            ></Message>
-                        </>
+                {unReadMessages.map((current) => (
+                    <Message
+                        key={current.id}
+                        sender={current.user}
+                        content={current.content}
+                        date={new Date(current.created_at).toTimeString().slice(0, 5)}
+                        sent={current.userId == user.id}
+                    />
+                ))}
 
-                    )
-                })}
-                {unReadMessages.length ? <div className="row">
-                    <h2 style={{
-                        background: '#E74C3C',
-                        borderRadius: '10px',
-                        paddingLeft: '3px',
-                        paddingRight: '3px',
-                    }}>Nouveaux</h2>
-                    <div className="horizontal-separator" style={{
-                        background: '#E74C3C',
-                        alignSelf: 'center',
-                        flexShrink: 0,
-                        flexGrow: 1,
-                    }}></div>
-                </div>: null}
-                {messages.filter(current => {
-                    return (current.id <= lastRead)
-                }).map((current) => {
-                    return (
-                        <>
-                            <Message
-                                key={current.id}
-                                sender={current.user}
-                                content={current.content}
-                                date={new Date(current.created_at).toTimeString().slice(0,5)}
-                                sent={current.userId == user.id}
-                            ></Message>
-                        </>
+                {unReadMessages.length ?
+                    <div className="row">
+                        <h2 style={{
+                            background: '#E74C3C',
+                            borderRadius: '10px',
+                            paddingLeft: '3px',
+                            paddingRight: '3px',
+                        }}>Nouveaux</h2>
+                        <div className="horizontal-separator" style={{
+                            background: '#E74C3C',
+                            alignSelf: 'center',
+                            flexShrink: 0,
+                            flexGrow: 1,
+                        }}></div>
+                    </div>
+                    : null}
 
-                    )
-                })}
-
-
+                {messages.filter(current => current.id <= lastRead).map((current) => (
+                    <Message
+                        key={current.id}
+                        sender={current.user}
+                        content={current.content}
+                        date={new Date(current.created_at).toTimeString().slice(0, 5)}
+                        sent={current.userId == user.id}
+                    />
+                ))}
 
             </div>
+
             {props.channel.conv === false && (
                 <div className="conv-false-content">
-                    {}
                     This is the content when 'conv' is false.
                 </div>
-                )}
+            )}
+
             <TextInput ref={ref} color="#7F8C8D" text="Votre message..." bgColor="#ECF0F1" onKeyDown={handleSendMessage} button={<Send handleClick={handleSendMessage}></Send>}></TextInput>
         </div>
     )
