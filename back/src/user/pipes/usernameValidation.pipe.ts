c3215b248b73c85bd80e4703d1b6a28a6f014a1d
esanchez <esanchez@student.42lausanne.ch>
2023-09-06 11:56:15 +0200

    create a global pipe for username validation

--- back/src/user/pipes/usernameValidation.pipe.ts
+++ back/src/user/pipes/usernameValidation.pipe.ts
@@ -1,22 +1,23 @@
-import {
-  PipeTransform,
-  Injectable,
-  ArgumentMetadata,
-  BadRequestException,
-} from "@nestjs/common";
+import { Injectable, PipeTransform, BadRequestException } from "@nestjs/common";
 import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
-export class UsernameAlreadyExistsPipe implements PipeTransform {
+export class UsernameValidationPipe implements PipeTransform {
   constructor(private readonly prisma: PrismaService) {}
 
-  async transform(value: any, metadata: ArgumentMetadata) {
+  async transform(value: any) {
+    if (typeof value !== 'string' || value.length > 16) {
+      throw new BadRequestException("Username must be 16 characters or fewer");
+    }
+
     const user = await this.prisma.user.findUnique({
       where: { username: value },
     });
     if (user) {
       throw new BadRequestException("Username already exists");
     }
+
     return value;
   }
 }
+
