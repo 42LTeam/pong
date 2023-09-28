b9b1ff1639f642d3db56062353f3d90dad6e8799
Nicolas Cotte <ncotte@c3r3s5.42lausanne.ch>
2023-09-28 15:02:39 +0200

    fix ip address script

--- ip_address.sh
+++ ip_address.sh
@@ -2,16 +2,12 @@
 
 IP_ADDRESS=$(ifconfig | grep -Eo "inet 10\.[0-9]+\.[0-9]+\.[0-9]+" --max-count=1 | awk '{print $2}')
 
-echo "IP_ADDRESS = " $IP_ADDRESS
-
 FIRST_OCTET=$(echo $IP_ADDRESS | cut -d "." -f 1)
 SECOND_OCTET=$(echo $IP_ADDRESS | cut -d "." -f 2)
 THIRD_OCTET=$(echo $IP_ADDRESS | cut -d "." -f 3)
 FOURTH_OCTET=$(echo $IP_ADDRESS | cut -d "." -f 4)
 
 LOCALHOST="${FIRST_OCTET}.${SECOND_OCTET}.${THIRD_OCTET}.${FOURTH_OCTET}"
-
-echo "LOCALHOST = " $LOCALHOST
 
 if grep -q "LOCALHOST=" .env; then
     sed -i.bak "s/LOCALHOST=.*/LOCALHOST=$LOCALHOST/" .env
