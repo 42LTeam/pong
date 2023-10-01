b724605fcc9440f8e89800a265e471d04604b55c
Shai <shai.halimi@gmail.com>
2023-10-01 17:10:00 +0200

    fixed ids error

--- back/src/channel/channel.service.ts
+++ back/src/channel/channel.service.ts
@@ -91,7 +91,7 @@ export class ChannelService {
         await this.addInvite(channelId, i);
       }
     }
-
+    if (ids)
     for (const i of ids) {
       await this.addInvite(channelId, i);
     }
